f24cabb4de63ec831e477003b430745ed7163577
2578313972 <2578313972@qq.com>
2020-03-13 18:02:04 +0800

    第三次提交

---
+++ uni-shopping/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="main">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped>
+	veiw{box-sizing: border-box;}
+	
+	.main{margin: 0upx;}
+</style>
